0620b7eae2e0e58f0a86646d5b1f02d2b5e10c15
fedeziraldo <federrico04@gmail.com>
2018-12-08 19:51:51 -0300

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+const index = require("./index");
 const app = express();
 app.use(index);
 const server = http.createServer(app);
