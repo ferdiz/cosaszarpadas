4648919c0959d60904c6fe322d90a9c67991dc68
Rafa <rafazira83@gmail.com>
2018-12-08 21:01:22 -0300

    cambios servidor

--- app.js
+++ app.js
@@ -7,18 +7,12 @@ const app = express();
 app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server);
+
 io.on("connection", socket => {
-  console.log("New client connected"), setInterval(
-    () => getApiAndEmit(socket),
-    10000
-  );
+  console.log("New client connected");
+   socket.on('chat',mensaje=>{socket.broadcast.emit('chat',mensaje)})
+  
   socket.on("disconnect", () => console.log("Client disconnected"));
 });
-const getApiAndEmit = async socket => {
-  try {
-    socket.emit("FromAPI", "puto");
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
-};
+
 server.listen(port, () => console.log(`Listening on port ${port}`));
